bdb86730a2f25897bb301e9c7a14f99f1e318313
Daniel Hassler <dwh714@verizon.net>
2023-10-06 12:11:47 -0400

    added tensor -> image conversion for torch.attack framework

--- attacks-torch.py
+++ attacks-torch.py
@@ -17,11 +17,18 @@ import art.attacks.evasion as evasion
 from art.estimators.classification import PyTorchClassifier
 
 class PILToFloatTensor(object):
+    '''
+    Converts integer representation of PIL into float. Used in Transform.
+    '''
     def __call__(self, img):
         # Convert PIL image to a float tensor
         return transforms.functional.to_tensor(img).float()
 
 def normalize_between_range(img_tensor):
+    '''
+    Normalizes a tensor to the range [0,1], important
+    for tensor input into the attack framework.
+    '''
     input_tensor = img_tensor
     min_value = 0.0
     max_value = 1.0
@@ -31,16 +38,39 @@ def normalize_between_range(img_tensor):
     normalized_tensor = normalized_tensor * (max_value - min_value) + min_value
     return normalized_tensor
 
-if __name__ == "__main__":
-    model = models.resnet18(pretrained=True)
-    model.to("cpu")
-    df = pd.read_csv("/home/grads/hassledw/StyleCLIP_Defense/FFHQ-512-labeled.csv")
-    df = df[:10] # first 10
-    label_encoder = LabelEncoder()
-    encoded_labels = label_encoder.fit_transform(df["expression"])
-    df["expression"] = encoded_labels
+def tensor_to_image(tensor_imgs):
+    '''
+    A helper function that converts a transformed adversarial tensor 
+    into an image that visualizable.
 
-    labels = torch.tensor(df["expression"].values)
+    tensor_imgs: tensors of images
+    '''
+    images = []
+    for tensor in tensor_imgs:
+        # Step 1: Reverse normalization to [0, 1]
+        min_value = 0.0
+        max_value = 1.0
+        tensor = (tensor - min_value) / (max_value - min_value)
+        tensor *= 255.0
+
+        tensor = tensor.to(torch.uint8)
+        numpy_image = tensor.cpu().numpy()
+
+        pil_image = Image.fromarray(numpy_image.transpose(1, 2, 0))  # Channels last
+
+        images.append(pil_image)
+
+    return images
+
+def generate_attack(attack, df, labels):
+    '''
+    Uses torch.attacks attack method to create adversarial image given the
+    specified attack, dataframe, and labels.
+
+    attack: framework (FGSM, PGD, AutoAttack ...)
+    df: image data in which to attack in the type of pd.dataframe with "image" column
+    labels: an encoding of the labels (integer values)
+    '''
     images = []
 
     for file_name in df["image"]: 
@@ -54,9 +84,26 @@ if __name__ == "__main__":
         images.append(normalize_between_range(input_tensor))
 
     # everything but JSMA and SPSA works so far.
-    atk = AutoAttack(model)
     images = torch.stack(images)
-    adv_images = atk.forward(images, labels)
+    adv_tensors = attack.forward(images, labels)
+    adv_images = tensor_to_image(adv_tensors)
+    return adv_images
+
+def main():
+    model = models.resnet18(pretrained=True)
+    model.to("cpu")
+
+    df = pd.read_csv("/home/grads/hassledw/StyleCLIP_Defense/FFHQ-512-labeled.csv")
+    df = df[:10] # first 10
+    label_encoder = LabelEncoder()
+    encoded_labels = label_encoder.fit_transform(df["expression"])
+    df["expression"] = encoded_labels
+
+    labels = torch.tensor(df["expression"].values)
+    adv_images = generate_attack(FGSM(model), df, labels)
     print(adv_images)
-    
-    
+
+    adv_images[0].save("Test.jpg")
+
+if __name__ == "__main__":
+    main()
